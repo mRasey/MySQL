fb447130490a767f77c23981082a6bd18f164b8f
mRasey <billywzh@126.com>
2016-11-19 11:29:58 +0800

    数据库大作业

---
+++ out/artifacts/MySQL_war_exploded/index.jsp
@@ -0,0 +1,105 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Billy
+  Date: 2016/9/25
+  Time: 13:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+      <title>医院信息管理系统</title>
+      <link rel="stylesheet" type="text/css" href="css/css-login/login.css" >
+      <link rel="stylesheet" type="text/css" href="css/css-login/htmleaf-demo.css" >
+      <link rel="stylesheet" type="text/css" href="css/css-login/normalize.css" >
+      <link rel="stylesheet" type="text/css" href="css/css-select/cs-select.css" >
+      <link rel="stylesheet" type="text/css" href="css/css-select/cs-skin-border.css" >
+
+      <script type="text/javascript" src="js/jquery-3.1.0.js"></script>
+      <script type="text/javascript" src="js/select/classie.js"></script>
+      <script type="text/javascript" src="js/select/selectFx.js"></script>
+      <script>
+          (function() {
+              [].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {
+                  new SelectFx(el);
+              } );
+          })();
+      </script>
+      <script type="text/javascript">
+          function check_login() {
+              var name=$("#user_name").val();
+              var pass=$("#password").val();
+
+              if(name == "" || pass == "") {
+                  $("#login_form").removeClass('shake_effect');
+                  setTimeout(function() {
+                      $("#login_form").addClass('shake_effect')
+                  },1);
+              }
+              else {
+                  $.ajax({
+                      type: "GET",
+                      dataType: "json",
+                      data: {
+                          "name": name,
+                          "password": pass
+                      },
+                      url: "CheckLogin"
+                  }).done(function (data) {
+//                  alert("done");
+//                      var ifFind = data.toString();
+                      var ifFind = eval(data).ifFind;
+                      if (ifFind == "false") {
+//                          alert("未注册，请注册后登陆");
+                          document.getElementById("hint").style.display = "";
+                          $("#login_form").removeClass('shake_effect');
+                          setTimeout(function () {
+                              $("#login_form").addClass('shake_effect')
+                          }, 1);
+                      }
+                      else if(ifFind == "error"){
+                          alert("发生错误");
+                          self.location = "index.jsp";
+                      }
+                      else {
+                          alert("欢迎登陆");
+                          self.location = "/users/" + ifFind +  "/index.jsp";
+                      }
+                  }).fail(function () {
+                      alert("fail");
+                      self.location = "index.jsp";
+                  });
+              }
+          }
+
+          $(function(){
+              $("#login").click(function(){
+                  check_login();
+                  return false;
+              });
+              $('.message a').click(function () {
+                  self.location = "register.jsp";
+              });
+          })
+      </script>
+  </head>
+  <body>
+      <div class="htmleaf-container">
+          <header class="htmleaf-header">
+              <h1>医院信息管理系统</h1>
+          </header>
+          <div id="wrapper" class="login-page">
+              <div id="login_form" class="form">
+                  <form class="login-form">
+                      <input type="text" placeholder="用户名" id="user_name"/>
+                      <input type="password" placeholder="密码" id="password"/>
+                      <button id="login">登　录</button>
+                      <p class="message">还没有账户? <a href="#">立刻创建</a></p>
+                      <p class="message" style="display: none" id="hint">
+                          <span style="color: red; ">账号不存在或密码错误</span></p>
+                  </form>
+              </div>
+          </div>
+      </div>
+  </body>
+</html>
---
+++ src/com/billy/servlets/CheckLogin.java
@@ -0,0 +1,140 @@
+package com.billy.servlets;
+
+import net.sf.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+import static com.billy.util.AddsingleQuotes.$;
+
+public class CheckLogin extends HttpServlet {
+
+    private Connection connection;
+    private Statement statement;
+    private String sql;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/医院信息管理系统?user=root&password=199507wz";
+            connection = DriverManager.getConnection(url);
+            if (!connection.isClosed())
+                System.out.println("数据库连接成功!");
+            statement = connection.createStatement();
+        }
+        catch(SQLException e) {
+            System.out.println("error");
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        JSONObject jsonObject = new JSONObject();
+        System.err.println("in checkLogin");
+        String name = req.getParameter("name");
+        String password = req.getParameter("password");
+        System.out.println(name + " " + password);
+        try {
+            sql = "SELECT 账号, 密码, 工作类别 FROM 员工 WHERE 账号 = " + $(name);
+            ResultSet resultSet = statement.executeQuery(sql);
+            if(resultSet.next()) {
+                if(resultSet.getString("密码").equals(password)) {
+//                    System.out.println(resultSet.getString("工作类别"));
+                    String s = resultSet.getString("工作类别");
+                    jsonObject.put("ifFind", s);
+                    resp.getWriter().print(jsonObject);
+//                    resp.getWriter().print("true");
+                    return;
+                }
+            }
+            jsonObject.put("ifFind", "false");
+            resp.getWriter().print(jsonObject);
+//            resp.getWriter().print("false");
+            System.out.println("false");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("操作数据库出错");
+            jsonObject.put("ifFind", "error");
+            resp.getWriter().print(jsonObject);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        JSONObject jsonObject = new JSONObject();
+        System.err.println("in checkRegister");
+        String name;
+        String password;
+        String job;
+        String email;
+        String realName;
+        String phone;
+        String address;
+
+        if(req.getParameter("op").equals("checkIfExist")) {
+            System.out.println("in checkIfExist");
+            name = req.getParameter("name");
+            password = req.getParameter("password");
+            System.out.println(name + " " + password);
+
+            try {
+                sql = "SELECT 账号 FROM 员工 WHERE 账号 = " + $(name);
+                ResultSet resultSet = statement.executeQuery(sql);
+                if (resultSet.next()) {
+                    resp.getWriter().print("true");
+                    return;
+                }
+                resp.getWriter().print("false");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("操作数据库出错");
+                resp.getWriter().print("error");
+            }
+        }
+        else if(req.getParameter("op").equals("submitAll")) {
+            try {
+                System.out.println("in submitAll");
+                name = req.getParameter("name");
+                password = req.getParameter("password");
+                job = req.getParameter("job");
+                email = req.getParameter("email");
+                realName = req.getParameter("realName");
+                phone = req.getParameter("phone");
+                address = req.getParameter("address");
+                sql = "INSERT " +
+                        "INTO 员工 " +
+                        "VALUES (" + $(name) + ","
+                                    + $(password) + ","
+                                    + $(job) + ","
+                                    + $(email) + ","
+                                    + $(realName) + ","
+                                    + $(phone) + ","
+                                    + $(address) + ");";
+                statement.executeUpdate(sql);
+                resp.getWriter().print("true");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                resp.getWriter().print("error");
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
